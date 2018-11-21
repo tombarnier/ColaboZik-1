d84baf288828b9f8b11a4826f53aa56d8b89accd
tombarnier <tom.barnier@ynov.com>
2018-11-21 15:03:48 +0100

    oups rip le homard

--- client/App.js
+++ client/App.js
@@ -1,24 +1,16 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
 import { Provider } from 'react-redux'
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, {Component} from 'react'
 
 import StackNavigator from './app/config/routes'
 import { store } from './app/config/store'
 
-type Props = {};
+type Props = {}
 export default class App extends Component<Props> {
   render() {
     return (
       <Provider store={store}>
         <StackNavigator />
       </Provider>
-    );
+    )
   }
 }
--- client/app/screens/home.js
+++ client/app/screens/home.js
@@ -1,6 +1,18 @@
 import { View, Text } from 'react-native'
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const BackgroundView = styled.View`
+  flex: 1;
+`
+
+const ContentContainer = styled.View`
+  background-color: ${props => props.theme.color.primary};
+  flex: 5;
+  justify-content: center;
+  align-items: center;
+`
 
 export default class Home extends Component {
   static propTypes = {
@@ -9,9 +21,9 @@ export default class Home extends Component {
 
   render() {
     return (
-      <View>
-        <Text>Homepage</Text>
-      </View>
+      <BackgroundView>
+      
+      </BackgroundView>
     )
   }
 }
