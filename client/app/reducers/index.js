52feed99d0c1d7bbd103cc833de75e7fc65a8c9e
Victor Gabou <victor.gabou@outlook.fr>
2018-11-22 17:00:33 +0100

    remove old import

--- client/app/reducers/index.js
+++ client/app/reducers/index.js
@@ -3,14 +3,11 @@ import { combineReducers } from 'redux'
 import users from './users'
 import playlists from './playlists'
 import auth from './auth'
-import feathers from './feathers'
 import musics from './musics'
 
 export default combineReducers({
   users,
   playlists,
   auth,
-  feathers,
   musics
 })
-
