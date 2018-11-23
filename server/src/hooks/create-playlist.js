abd9a2a52b4c9ecd317df4738a3a7b1e8d2c07b6
tombarnier <tom.barnier@ynov.com>
2018-11-23 17:01:57 +0100

    bugfix par le bas

--- server/src/hooks/create-playlist.js
+++ server/src/hooks/create-playlist.js
@@ -2,30 +2,8 @@
 
 module.exports = function (options = {}) {
   return async context => {
-    await new Promise((resolve,reject) => {
-      const { app, data } = context;
-      app.service('playlists').find(data.name).then(async (res) => {
-        if (res.total > 0){
-          reject(new Error('playlist existante'));
-        }
-        else {
-          if (!data.musics){
-            let musics =[];
-            await data.musics.map((music) => {
-              app.service('musics').find(music.id).then((res) =>{
-                if(res.total > 0) musics.push(res.data[0]._id);
-                else console.log('musique inexistante');
-              });
-              context.musics = musics; 
-            });
-          }
-          data.tags = data.tags.split(' ');
-          resolve();
-        }
-      })
-        .catch((err) => {throw err;});
-    })
-      .then(() => {return context;})
-      .catch((err) => {throw err;});
-  };
-};
+    const { app, data } = context;
+    data.tags = data.tags.split(' ')
+    return context
+  }
+}
