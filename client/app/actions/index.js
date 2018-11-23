0bf1bf66702f85d65dcac154955fa15534345e83
Victor Gabou <victor.gabou@outlook.fr>
2018-11-23 03:51:00 +0100

    remove unused files

--- client/app/actions/index.js
+++ client/app/actions/index.js
@@ -1,11 +1,5 @@
-import * as users from './users'
-import * as musics from './musics'
-import * as playlists from './playlists'
 import * as auth from './auth'
 
 export default {
-  users,
-  playlists,
-  auth,
-  musics
+  auth
 }
--- client/app/reducers/index.js
+++ client/app/reducers/index.js
@@ -1,13 +1,7 @@
 import { combineReducers } from 'redux'
 
-import users from './users'
-import playlists from './playlists'
 import auth from './auth'
-import musics from './musics'
 
 export default combineReducers({
-  users,
-  playlists,
-  auth,
-  musics
+  auth
 })
