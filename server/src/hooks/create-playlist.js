9b2dd534c57e053d055732360bb65ff741cd3de3
LeoAttn <leo.massonnat@hotmail.fr>
2018-11-25 23:00:36 +0100

    fix(server/create-playlist): ne créé plus de tag vide si la string tag est vide et supprime les espaces avant/apres le nom

--- server/src/hooks/create-playlist.js
+++ server/src/hooks/create-playlist.js
@@ -1,9 +1,11 @@
-const strToArray = (str) => [...new Set(str.trim().split(/\s+/))];
+// Transforme une string en tableau de strings en supprimant les doublons et les strings vides
+const strToArray = (str) => [...new Set(str.split(/\s+/).filter(x => x))];
 
 module.exports = function(options = {}) {
   return async context => {
     const { app, data } = context;
     data.tags = strToArray(data.tags);
+    data.name = data.name.trim();
     return context;
   };
 };
