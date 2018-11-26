a1cf059256477b010b77b51a4fa21d2ca967c02d
zoubi <moricio123@protonmail.com>
2018-11-27 00:37:56 +0100

    need to test on a real phone, doesnt work on VD

--- client/app/screens/player.js
+++ client/app/screens/player.js
@@ -3,6 +3,7 @@ import { Dimensions, Text, TouchableOpacity, View, WebView } from 'react-native'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import PropTypes from 'prop-types'
+import {Fab, Icon} from 'native-base'
 
 
 import allTheActions from '../actions'
@@ -23,7 +24,8 @@ class Player extends Component {
   state = {
     playlist: [],
     id: 0,
-    url: ''
+    url: '',
+    title: ''
   }
   componentDidMount() {
     const { navigation, actions } = this.props
@@ -32,7 +34,7 @@ class Player extends Component {
     actions.musics.loadMusic(playlist._id)
     this.setState(() => ({ playlist: this.props.musics }))
     this.setState(() => ({ url: this.props.musics[0].embed }))
-
+    this.setState(() => ({ title: this.props.musics[0].title }))
   }
 
   componentWillUnmount() {
@@ -40,7 +42,6 @@ class Player extends Component {
 
     actions.musics.unloadMusic()
   }
-
   nextMusic = () => {
     let nextMusic = 0
     if (this.state.id != this.state.playlist.length - 1)
@@ -48,48 +49,75 @@ class Player extends Component {
     else
       nextMusic = this.state.playlist[0]
     let url = nextMusic.embed
-    let id = parseInt(nextMusic.id) - 1
+    let id = this.state.playlist.indexOf(nextMusic)
+    let title = nextMusic.title
+    console.log(title)
+
     this.setState({ id: id })
     this.setState({ url: url })
+    this.setState({ title: title })
+    console.log(nextMusic)
   }
   prevMusic = () => {
     let prevMusic = 0
     if (this.state.id != 0)
       prevMusic = this.state.playlist[this.state.id - 1]
     else
-      prevMusic = this.state.playlist[this.playlist.length - 1]
+      prevMusic = this.state.playlist[this.state.playlist.length - 1]
     let url = prevMusic.embed
-    let id = parseInt(prevMusic.id) - 1
+    let id = this.state.playlist.indexOf(prevMusic)
+    let title = prevMusic.title
     this.setState({ id: id })
     this.setState({ url: url })
+    this.setState({ title: title })
   }
 
   render() {
+    const {title} = this.state.title
     return (
       <View>
-        <View style={{ height: ScreenHeight - 100, width: ScreenWidth }}>
+        <View style={{ height: ScreenHeight - 175, width: ScreenWidth }}>
           <WebView style={{
             paddingTop: 25,
             backgroundColor: '#f8f8f8',
             width: ScreenWidth,
-            height: ScreenHeight
+            height: ScreenHeight,
           }}
-                   source={{ uri: this.state.url }}/>
+                  source={{ uri: this.state.url }}
+                  mediaPlaybackRequiresUserAction={false}
+                  javaScriptEnabled={true}
+                  scalesPageToFit={true}
+                  />
         </View>
         <View style={{
           backgroundColor: '#131313',
-          height: 100,
+          height: 175,
           width: ScreenWidth
         }}>
           <View style={{
             width: ScreenWidth
           }}>
-            <TouchableOpacity onPress={() => this.nextMusic()}><Text
-              style={{ color: '#fff', padding: 10, fontSize: 15 }}>Next</Text></TouchableOpacity>
-            <TouchableOpacity onPress={() => this.prevMusic()}><Text
-              style={{ color: '#fff', padding: 10, fontSize: 15 }}>Prev</Text></TouchableOpacity>
+          <Fab
+            style={{backgroundColor: '#5067FF'}}
+            position="topRight"
+            onPress={() => this.nextMusic()}>
+            <Icon name="play"/>
+          </Fab>
+          <Fab
+          style={{backgroundColor: '#5067FF'}}
+          position="topLeft"
+          onPress={() => this.prevMusic()}>
+          <Icon name="play"/>
+        </Fab>
+        <Text style={{textAlign: 'center', // <-- the magic
+          fontWeight: 'bold',
+          fontSize: 18,
+          marginTop: 0,
+          marginLeft: 100,
+          marginRight: 100,
+          color:'white',
+          backgroundColor: '#131313',}}>{this.state.title}</Text>
           </View>
-          <Text> </Text>
         </View>
       </View>
     )
