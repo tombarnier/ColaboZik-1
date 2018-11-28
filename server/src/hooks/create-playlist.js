fc211a5daa4a6febc9c4ec6a334befa5222d5ca1
tombarnier <tom.barnier@ynov.com>
2018-11-28 11:46:14 +0100

    si une playlist a un nom vide il ne s'ajoute pas

--- server/src/hooks/create-playlist.js
+++ server/src/hooks/create-playlist.js
@@ -3,11 +3,11 @@ const strToArray = (str) => [...new Set(str.split(/\s+/).filter(x => x))];
 
 module.exports = function(options = {}) {
   return async context => {
-    const { app, data } = context;
-    const {name, tags} = data;
-
-    data.name = name.trim();
-    data.tags = tags ? strToArray(tags) : [];
+    const { app, data } = context
+    const {name, tags} = data
+    data.name = name.trim()
+    if (data.name === "") throw new Error('champ nom vide')
+    data.tags = tags ? strToArray(tags) : []
 
     return context;
   };
