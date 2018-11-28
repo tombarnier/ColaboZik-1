32ddee5af1803458d9ae17be5912fef633fd2c5b
tombarnier <tom.barnier@ynov.com>
2018-11-28 15:08:55 +0100

    Player youtube un peu buggy

--- client/app/screens/player.js
+++ client/app/screens/player.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
-import { Dimensions, Text, View, WebView } from 'react-native'
+import { Dimensions, Text, View, StyleSheet } from 'react-native'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import PropTypes from 'prop-types'
 import { Fab, Icon } from 'native-base'
+import YouTube from 'react-native-youtube'
+import { API_KEY_YT } from '../../config'
 
 import allTheActions from '../actions'
 
@@ -36,7 +38,6 @@ class Player extends Component {
       alert('Playlist vide !')
       return
     }
-
     this.setState(() => ({ playlist: musics }))
     this.setState(() => ({ url: musics[0].embed }))
     this.setState(() => ({ title: musics[0].title }))
@@ -53,12 +54,9 @@ class Player extends Component {
     let url = nextMusic.embed
     let nextId = playlist.indexOf(nextMusic)
     let title = nextMusic.title
-    console.log(title)
-
     this.setState({ id: nextId })
     this.setState({ url: url })
     this.setState({ title: title })
-    console.log(nextMusic)
   }
   prevMusic = () => {
     const { playlist, id } = this.state
@@ -80,50 +78,43 @@ class Player extends Component {
     const { title, url } = this.state
     return (
       <View>
-        <View style={{ height: ScreenHeight - 175, width: ScreenWidth }}>
-          <WebView style={{
-            paddingTop: 25,
-            backgroundColor: '#f8f8f8',
-            width: ScreenWidth,
-            height: ScreenHeight
+        <YouTube
+          videoId={url}   // The YouTube video ID
+          play={true}
+          controls={1}            // control playback of video with true/false
+          fullscreen={false}       // control whether the video should play in fullscreen or inline
+          loop={false}
+          apiKey={API_KEY_YT}           // control whether the video should loop when ended
+          onError={() => {
+            this.nextMusic()
           }}
-                   source={{ uri: url }}
-                   mediaPlaybackRequiresUserAction={false}
-                   javaScriptEnabled={true}
-                   scalesPageToFit={true}
-          />
-        </View>
+          style={{ alignSelf: 'stretch', height:'80%' }}
+        />
         <View style={{
-          backgroundColor: '#131313',
-          height: 175,
           width: ScreenWidth
         }}>
-          <View style={{
-            width: ScreenWidth
-          }}>
-            <Fab
-              style={{ backgroundColor: '#5067FF' }}
-              position='topRight'
-              onPress={() => this.nextMusic()}>
-              <Icon name='skip-forward'/>
-            </Fab>
-            <Fab
-              style={{ backgroundColor: '#5067FF' }}
-              position='topLeft'
-              onPress={() => this.prevMusic()}>
-              <Icon name='skip-backward'/>
-            </Fab>
-            <Text style={{
-              textAlign: 'center', // <-- the magic
-              fontWeight: 'bold',
-              fontSize: 18,
-              marginTop: 0,
-              marginLeft: 100,
-              marginRight: 100,
-              color: 'white',
-              backgroundColor: '#131313'
-            }}>{title}</Text>
-          </View>
+          <Fab
+            style={{ backgroundColor: '#5067FF' }}
+            position='topRight'
+            onPress={() => this.nextMusic()}>
+            <Icon name='skip-forward'/>
+          </Fab>
+          <Fab
+            style={{ backgroundColor: '#5067FF' }}
+            position='topLeft'
+            onPress={() => this.prevMusic()}>
+            <Icon name='skip-backward'/>
+          </Fab>
+          <Text style={{
+            textAlign: 'center', // <-- the magic
+            fontWeight: 'bold',
+            fontSize: 18,
+            marginTop: 0,
+            marginLeft: 100,
+            marginRight: 100,
+            color: 'white',
+            backgroundColor: '#131313'
+          }}>{title}</Text>
         </View>
       </View>
     )
