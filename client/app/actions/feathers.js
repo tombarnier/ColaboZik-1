3c1ee3fed7c3f8a56929711347728d23c1f45dd9
Victor Gabou <victor.gabou@outlook.fr>
2018-11-29 10:40:17 +0100

    fix(user store): user wasn't set

--- client/app/actions/feathers.js
+++ client/app/actions/feathers.js
@@ -32,14 +32,14 @@ export const reauthenticate = () => dispatch => {
       return app.passport.verifyJWT(response.accessToken)
     })
     .then(payload => {
-      app.service('users').get(payload.userId).then((user) => {
+      return app.service('users').get(payload.userId).then((user) => {
         dispatch(
           authenticate({
             user
           })
         )
+        return !!payload.userId
       })
-      return !!payload.userId
     }).catch((e) => {
       // console.log('error:', e)
       return false
