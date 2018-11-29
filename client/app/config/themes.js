817dfc541a65cdd15e22f805eb680028ae34bc9b
Victor Gabou <victor.gabou@outlook.fr>
2018-11-29 05:23:44 +0100

    add theme config file

---
+++ client/app/config/themes.js
@@ -0,0 +1,19 @@
+export const themeLight = {
+  color: {
+    font: '#000',
+    background: '#f2f2f2',
+    cards: '#fff',
+    tags: '#ffc107',
+    button: '#007bff'
+  }
+}
+
+export const themeDark = {
+  color: {
+    font: '#fff',
+    background: '#2d2d2d',
+    cards: '#000',
+    tags: '#ffc107',
+    button: '#007bff'
+  }
+}
